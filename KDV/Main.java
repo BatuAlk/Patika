995794cfef73c8ff95aab214c1a8507dbf94f0c6
BatuAlk <108514496+BatuAlk@users.noreply.github.com>
2022-07-25 15:43:15 +0300

    Update Main.java

--- KDV/Main.java
+++ KDV/Main.java
@@ -1,1 +1,28 @@
+import java.util.Scanner;
 
+/* 
+ *  @author: Batuhan Efe Alkış
+ * 
+ * 	Patika Java101 KDV projesi
+ * */
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int para;
+
+		System.out.println("KDV'si hseaplanacak ücreti giriniz: ");
+		para = scan.nextInt();
+
+		float kdv = 0;
+		if (0 < para && para < 1000) {
+			kdv = 0.18f;
+		} else if (para >= 1000) {
+			kdv = 0.08f;
+		}
+
+		scan.close();
+		System.out.println("KDV'li para: " + (para + para * kdv));
+	}
+}
