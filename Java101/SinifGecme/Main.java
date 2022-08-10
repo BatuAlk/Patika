d8c3ca17b1000968fac75c654f6870bcd6252a62
BatuAlk <108514496+BatuAlk@users.noreply.github.com>
2022-08-10 16:56:21 +0300

    Update and rename SinifGecme/Main.java to Java101/SinifGecme/Main.java

--- Java101/SinifGecme/Main.java
+++ Java101/SinifGecme/Main.java
@@ -1,11 +1,5 @@
 import java.util.Scanner;
 
-/* 
- *  @author: Batuhan Efe Alkış
- * 
- * 	Patika Java101 kullanıcı girişi
- * 
- */
 
 public class Main {
 	public static void main(String[] args) {
